c273273006bd9c10ff39ec6506e84e7a807af0ca
jeremy <jeremyfelix41@gmail.com>
2023-09-14 14:30:05 +0200

    Création de l'interface d'accueil stylisée. Les recettes sont rajoutées dynamiquement par l'utilisateur. Le site est responsive.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css'
+// import './style.css'
 
 const addButton = document.querySelector("#add_button") as HTMLButtonElement;
 const recipeNameField = document.querySelector("#recipe_name_field") as HTMLInputElement;
@@ -6,15 +6,56 @@ const recipeLinkField = document.querySelector("#recipe_link_field") as HTMLInpu
 const recipeDurationField = document.querySelector("#recipe_duration_field") as HTMLInputElement;
 const recipeNoteField = document.querySelector("#recipe_note_field") as HTMLInputElement;
 
+let idCompteur = 0;
 
+addButton.addEventListener("click", function(event){
 
-addButton.addEventListener("click", function(){
-  console.log("j'ai cliqué sur le bouton !");
+  if(recipeNameField.value != "" && recipeNoteField.value != "" && recipeDurationField.value && recipeLinkField.value != "") {
 
-  /*
-  const recipeContenair = document.createElement("section");
-  archivedTaskBlock?.setAttribute('class', 'recipe_contenairs');
-  document.querySelector('#app')?.appendChild(recipeContenair);
-  */
+    if(idCompteur == 0) {
+      const myRecipesTitle = document.createElement("h2");
+      myRecipesTitle.innerText = "Mes recettes";
+      document.querySelector('#app')?.appendChild(myRecipesTitle);
+    }
 
+    event.preventDefault() // Annulation du comportement par défault du formulaire (qui ne doit pas être envoyé dans le cadre de ce projet)
+  
+    let idNewRecipe = "recipe_" + idCompteur;
+    idCompteur++;
+  
+    console.log(idNewRecipe);
+    
+    const recipeContenair = document.createElement("section");
+    recipeContenair?.setAttribute('id', idNewRecipe);
+    recipeContenair?.setAttribute('class', 'recipe_contenairs');
+    document.querySelector('#app')?.appendChild(recipeContenair);
+    
+    const recipeDescription = document.createElement("article");
+    recipeDescription?.setAttribute('class', 'recipe_descriptions column_recipes');
+    document.querySelector("#" + idNewRecipe)?.appendChild(recipeDescription);
+  
+    const recipeTitleResult = document.createElement("h3");
+    recipeTitleResult.innerText = recipeNameField.value;
+    document.querySelector("#" + idNewRecipe + " .recipe_descriptions")?.appendChild(recipeTitleResult);
+  
+    const recipeNoteResult = document.createElement("p");
+    recipeNoteResult.innerText = "Note : " + recipeNoteField.value;
+    document.querySelector("#" + idNewRecipe + " .recipe_descriptions")?.appendChild(recipeNoteResult);
+  
+    const recipeDurationResult = document.createElement("p");
+    recipeDurationResult.innerText = "Durée : " + recipeDurationField.value + " minutes";
+    document.querySelector("#" + idNewRecipe + " .recipe_descriptions")?.appendChild(recipeDurationResult);
+  
+    
+    const recipeImageFrame = document.createElement("article");
+    recipeImageFrame?.setAttribute('class', 'recipe_image_frames column_recipes');
+    recipeImageFrame.style.backgroundImage = "url(" + recipeLinkField.value + ")";
+    document.querySelector("#" + idNewRecipe)?.appendChild(recipeImageFrame);
+
+    // Effacement des champs du formulaire
+    recipeNameField.value = "";
+    recipeNoteField.value = "";
+    recipeDurationField.value = "";
+    recipeLinkField.value = "";
+  }
 });
